53645c91635b46b66568459ed6e69af3668e5006
Mappel21 <Mappel7676@gmail.com>
2021-06-16 23:50:33 +0000

    class commit

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 let pronoun = ['the','our'];
-  let adj = ['great', 'big' ];
-  let noun = ['jogger','racoon'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+   function domainName() {
+let myArray= []
+    for (let i = 0; i < pronoun.length; i++) {
+      
+        for (let j = 0; j < adj.length; j++) {
+            
+            for (let n = 0; n < noun.length; n++) {
+                 
+                myArray.push(pronoun[i] + adj[j] + noun[n] + ".com"+"<br>");            
+            }
+        }
+        
+    }
+    document.querySelector('.name').innerHTML= myArray
+ }
